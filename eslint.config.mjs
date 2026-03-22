4b2b142892d46e413011c4a23ac2ac523217471a
frsqredm <fr.sqre.dm@gmail.com>
2026-03-23 00:19:01 +0700

    chore: add eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+
+import { defineConfig } from "eslint/config";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default defineConfig(
+	eslint.configs.recommended,
+	tseslint.configs.recommended,
+	eslintConfigPrettier,
+);
