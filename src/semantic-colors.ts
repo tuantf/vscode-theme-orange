e51c92eca08e591cbcc44256d59a5bcb3fa48eb2
frsqredm <fr.sqre.dm@gmail.com>
2026-03-27 00:35:59 +0700

    refactor: simplify property colors and add json support

--- src/semantic-colors.ts
+++ src/semantic-colors.ts
@@ -1,11 +1,17 @@
 import { allColors, ColorScheme } from "./shared";
 
 export const getSemanticColors = (scheme: ColorScheme) => {
-	const { orange, blue, fg1, green, mauve } = allColors[scheme];
+	const { orange, fg1, fg2, green, mauve } = allColors[scheme];
 	return {
 		"constant.builtin": mauve,
-		property: blue,
+		property: fg1,
 		"property:python": fg1,
+		"property:typescript": fg2,
+		"property:javascript": fg2,
+		"property:javascriptreact": fg2,
+		"property:typescriptreact": fg2,
+		"property:json": green,
+		"property:jsonc": green,
 		parameter: fg1,
 		variable: fg1,
 		function: green,
